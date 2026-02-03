a2a5e2892ef1ff4ac35749b4b3b75f9057d57215
kokorocks <175902687+kokorocks@users.noreply.github.com>
2026-02-03 09:04:18 -0500

    added some styles

---
+++ public/scripts/appearence.js
@@ -0,0 +1,16 @@
+let appIconShapes='squircle'
+appIconShapes = 'circle'
+switch (appIconShapes) {
+    case 'squircle':
+        //document.documentElement.style.setProperty('--app-icon-border-radius', '20%');
+        break;
+    case 'circle':
+        document.documentElement.style.setProperty('--app-icon-border-radius', '50%');
+        break;
+    case 'hexagon':
+        document.documentElement.style.setProperty('--app-icon-border-radius', '0%');
+        document.querySelectorAll('.app-icon').forEach(icon => {
+            icon.style.clipPath = 'polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)';
+        });
+        break;
+}
