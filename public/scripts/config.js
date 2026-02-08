45afda30f06f8104580dc53be71dc119a53068e7
kokorocks <175902687+kokorocks@users.noreply.github.com>
2026-02-08 15:50:14 -0500

    maybe?

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -39,6 +39,7 @@ const appDB = {
     'browser': { name: 'Browser', icon: 'img:assets/icons/browser.svg', app: 'browser/browser-app.htm', permissions: ['camera', 'microphone', 'clipboard-read', 'clipboard-write'] },
     'music': { name: 'Music', icon: 'fa-music' },
     'files': { name: 'Files', icon: 'fa-folder' },
+    'compass': { name: 'Compass', icon: 'fa-compass', app: 'compass.htm' },
     //'health': { name: 'Health', icon: 'fa-heart', color: '#FF2D55' },
     //'wallet': { name: 'Wallet', icon: 'fa-wallet', color: '#000' },
     'test': { name: 'Test App', icon: 'fa-vial', app: 'test.htm', permissions: ['camera', 'microphone', 'clipboard-read', 'clipboard-write'] }
